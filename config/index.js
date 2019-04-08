5e88e83f8b67efe9e781bf911d8ca976d329ae12
xyzshen <304565162@qq.com>
2019-04-08 17:02:17 +0800

    build
    Signed-off-by: xyzshen <304565162@qq.com>

--- config/index.js
+++ config/index.js
@@ -12,7 +12,8 @@ module.exports = {
         assetsPublicPath: '/',
         proxyTable: {
             '/mgr':{
-                target:'http://10.0.0.1/',
+                // target:'http://10.0.0.1/',
+                // target: 'http://127.0.0.1:8081',
                 changeOrigin:true,
                 pathRewrite:{
                     '^/mgr':''
