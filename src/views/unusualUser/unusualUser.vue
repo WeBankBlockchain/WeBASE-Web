a9259ecac9c2cb2052dffa44b837375bf4293f21
xyzshen <304565162@qq.com>
2019-04-11 14:49:05 +0800

    修改bug
    Signed-off-by: xyzshen <304565162@qq.com>

--- src/views/unusualUser/unusualUser.vue
+++ src/views/unusualUser/unusualUser.vue
@@ -25,7 +25,7 @@
                 </div>
             </div>
             <div class="search-table" style="height: 76%">
-                <el-table :data="hashData" tooltip-effect="light" height="100%" style="overflow-y: auto" v-loading="loading">
+                <el-table :data="hashData" class="unUser-table-content"  tooltip-effect="light" height="100%" style="overflow-y: auto" v-loading="loading">
                     <el-table-column type="expand">
                         <template slot-scope="props">
                             <div class="expand-item">
@@ -243,5 +243,8 @@ export default {
 .expand-item-span > span {
     color: #515356;
     margin-left: 47px;
+}
+.unUser-table-content>>>.el-icon-arrow-right:before {
+    content: "\e60e";
 }
 </style>
