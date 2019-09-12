8645b524cda97089a4ad92436fc1c964d778edc4
xyzshen <304565162@qq.com>
2019-09-12 16:12:05 +0800

    test new pr
    Signed-off-by: xyzshen <304565162@qq.com>

---
+++ src/views/permission/index.vue
@@ -0,0 +1,38 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: '',
+
+    components: {
+    },
+
+    props: {
+    },
+
+    data() {
+        return {
+        }
+    },
+
+    computed: {
+    },
+
+    watch: {
+    },
+
+    created() {
+    },
+
+    mounted() {
+    },
+
+    methods: {
+    }
+}
+</script>
+
+<style scoped>
+</style>
