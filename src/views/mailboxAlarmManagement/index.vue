73e972f04735786bea19c10a8da58550c49162ef
xyzshen <304565162@qq.com>
2019-09-29 18:48:31 +0800

    cert
    Signed-off-by: xyzshen <304565162@qq.com>

---
+++ src/views/mailboxAlarmManagement/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <v-content-head :headTitle="'告警配置'" :headSubTitle="'邮件告警配置'" @changGroup="changGroup"></v-content-head>
+        <div>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+import contentHead from "@/components/contentHead";
+export default {
+    name: 'MailboxAlarmManagement',
+
+    components: {
+        "v-content-head": contentHead,
+    },
+
+    props: {
+    },
+
+    data() {
+        return {
+        }
+    },
+
+    computed: {
+    },
+
+    watch: {
+    },
+
+    created() {
+    },
+
+    mounted() {
+    },
+
+    methods: {
+        changGroup() {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
