f7b2573536f227a2aa6dfd25601307642e9b06b3
xyzshen <304565162@qq.com>
2019-10-18 10:40:05 +0800

    修复权限管理没有私钥的时候展示地址 修复证书导出格式
    Signed-off-by: xyzshen <304565162@qq.com>

--- src/views/certificate/index.vue
+++ src/views/certificate/index.vue
@@ -155,21 +155,8 @@ export default {
             exportCert(list).then(res => {
                 const { data } = res;
                 if (data.code === 0) {
-                    const content = data.data.content;
-                    const blob = new Blob([content], { type: 'application/x-x509-ca-cert' });
-                    const fileName = data.data.certName;
-                    if ('download' in document.createElement('a')) { // 非IE下载
-                        const elink = document.createElement('a')
-                        elink.download = fileName
-                        elink.style.display = 'none'
-                        elink.href = URL.createObjectURL(blob)
-                        document.body.appendChild(elink)
-                        elink.click()
-                        URL.revokeObjectURL(elink.href) // 释放URL 对象
-                        document.body.removeChild(elink)
-                    } else { // IE10+下载
-                        navigator.msSaveBlob(blob, fileName)
-                    }
+                    let fileDownload = require('js-file-download');
+                    fileDownload( data.data.content ,`${data.data.certName}.crt`);
                 } else {
                     this.$message({
                         type: 'error',
